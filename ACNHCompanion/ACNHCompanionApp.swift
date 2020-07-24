46ff283cbe805c0b49f26355c95a90b1969a16a2
Samantha Emily-Rachel Belnavis <samanthabelnavis@gmail.com>
2020-07-23 23:35:24 -0400

    started xcode project

---
+++ ACNHCompanion/ACNHCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACNHCompanionApp.swift
+//  ACNHCompanion
+//
+//  Created by Samantha Emily-Rachel Belnavis on Jul 23 2020.
+//
+
+import SwiftUI
+
+@main
+struct ACNHCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
